3291c17383ce8119583ce1836a65510af1cff7ec
Eliezer Portilho <eliezerportilho@gmail.com>
2022-04-27 08:57:09 -0300

    Backend pronto até a aula 3

---
+++ src/controllers/CreateTagController.ts
@@ -0,0 +1,14 @@
+import { request, Request, Response } from 'express';
+import { CreateTagsService } from "../services/CreateTagsService"
+
+class CreateTagController{
+    async handle(request: Request, response: Response){
+        const { name } = request.body
+        const createTagService = new CreateTagsService()
+
+        const tag = await createTagService.execute(name)
+        return response.json(tag)
+    }
+}
+
+export { CreateTagController }
